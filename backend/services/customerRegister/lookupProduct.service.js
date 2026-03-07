52ec4c7e4c3ad6cdbbcee9fb8972e18dd4ec730d
Sujal Gujar <sujalgurjar919@gmail.com>
2026-03-07 18:48:40 +0530

    solve the error of product mongoose

--- backend/services/customerRegister/lookupProduct.service.js
+++ backend/services/customerRegister/lookupProduct.service.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose"
 
 // ─────────────────────────────────────────────────────────────────────────────
 //  FILE: services/productLookupService.js
