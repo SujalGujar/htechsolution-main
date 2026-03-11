6498e26945672ef2df0dd6268e41f4cc0f090895
Sujal Gujar <sujalgurjar919@gmail.com>
2026-03-11 17:33:58 +0530

    solve the deleted items return

--- backend/services/catogoryServices/getAllProducts.service.js
+++ backend/services/catogoryServices/getAllProducts.service.js
@@ -1,7 +1,15 @@
+// import productRegister from "../../models/productsRegisterModel/productRegister.model.js";
+
+// export const getAllProducts = async () => {
+//   return await productRegister.find({isActive:true})
+//     .populate("category")
+//     .sort({ createdAt: -1 });
+// };
+
 import productRegister from "../../models/productsRegisterModel/productRegister.model.js";
 
 export const getAllProducts = async () => {
-  return await productRegister.find()
+  return await productRegister.find({ isActive: true })  // ← add this filter
     .populate("category")
     .sort({ createdAt: -1 });
 };
